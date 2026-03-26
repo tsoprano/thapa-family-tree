9b4339f88d8cc782c0fecf5065fb0e1ae0f1b9df
Samyam Thapa <33021499+tsoprano@users.noreply.github.com>
2026-03-26 15:49:02 -0500

    Add files via upload

---
+++ family-tree/js/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://wjxvwicglqvkprvhnghh.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqeHZ3aWNnbHF2a3BydmhuZ2hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1NTcyNjIsImV4cCI6MjA5MDEzMzI2Mn0.MjUpI1mUyGE4ngoA0gvqocQW-o0UO77P0YL2pNGmQr4"
+);
